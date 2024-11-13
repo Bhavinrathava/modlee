8242456e6a7f45d98d473f87c2ff99b9e73732e7
mansiagr4 <mansi@modlee.ai>
2024-11-13 14:46:07 -0800

    minor changes

--- src/modlee/model_metafeatures.py
+++ src/modlee/model_metafeatures.py
@@ -21,7 +21,6 @@ class ModelMetafeatures:
 
         if sample_input is None:
             sample_input = INPUT_DUMMY[self.modality]
-            sample_input.to(dtype=torch.float32)
             print("Using default sample input")
         else:
             # Check the input type
@@ -30,14 +29,12 @@ class ModelMetafeatures:
             # Unpack if it's a list of length 1
             if isinstance(sample_input, list) and len(sample_input) == 1:
                 sample_input = sample_input[0]
-                sample_input.to(dtype=torch.float32)
                 #print(f"Unpacked sample_input: {type(sample_input)}")
 
             # Convert to tensor if it's not already a tensor
             if not torch.is_tensor(sample_input):
                 try:
                     sample_input = torch.tensor(sample_input)
-                    sample_input.to(dtype=torch.float32)
                     
                 except Exception as e:
                     print(f"Error converting sample_input to tensor: {e}")
