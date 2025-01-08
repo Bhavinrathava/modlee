bf8e43a37362d145b92523aa6f969a16c4e72db3
nik-modlee <nikhil@modlee.ai>
2025-01-08 06:43:33 +0000

    text to text

--- src/modlee/model/text_model.py
+++ src/modlee/model/text_model.py
@@ -51,7 +51,7 @@ class TextTextToTextModleeModel(ModleeModel):
 
         """
         modality = 'text'
-        task = "text-to-text"
+        task = "texttotext"
         vars_cache = {"modality":modality,"task": task}
         ModleeModel.__init__(
             self,
